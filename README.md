ffe749fd8bc60ecb36ad5cc3bcd973359362ee15
LRPalacios <lrpalaciosdev@gmail.com>
2016-05-17 10:02:35 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# web-project-template-less
+A web project template based on Jhon Papa pluralsight-gulp course but  stripped out of initial components it is truly an empty template ready for you to kick-start a project
+
+This template take advantage of JavaScript task automation using Gulp to streamline tasks such as compiling and injection less, installing and adding to you index.html trough bower with wiredep and optimization of you static assets for deploying
+
+## Requirements
+
+- Install Node
+	- on OSX install [home brew](http://brew.sh/) and type `brew install node`
+	- on Windows install [chocolatey](https://chocolatey.org/) 
+    - open command prompt as administrator
+        - type `choco install nodejs`
+        - type `choco install nodejs.install`
+- On OSX you can alleviate the need to run as sudo by [following these instructions](http://jpapa.me/nomoresudo). I highly recommend this step on OSX
+- Open terminal
+- Type `npm install -g node-inspector bower gulp`
+
+## Quick Start
+```bash
+$ npm install
+$ bower install
+$ npm start
+```
+
+## Tasks List
+1. gulp styles 
+2. gulp inject
+3. gulp wiredep
+4. gulp build
+5. gulp sync
+6. TODO..
+
+## Author
+Luis Palacios
+
+## TODO
+Need to provide a description of each task and a video tutorial how to start
+
---
+++ gulp.config.js
@@ -0,0 +1,94 @@
+module.exports = function () {
+    var client = './src/client/';
+    var clientApp = client + 'app/';
+    var report = './report/';
+    var root = './';
+    var server = './src/server/';
+    var temp = './.tmp/';
+    var wiredep = require('wiredep');
+    var bowerFiles = wiredep({
+        devDependencies: true
+    })['js'];
+
+    var config = {
+        /**
+         * Files paths
+         */
+        alljs: [
+            './src/**/*.js',
+            './*.js'
+        ],
+        build: './build/',
+        client: client,
+        css: temp + 'styles.css',
+        fonts: [
+            './bower_components/font-awesome/fonts/**/*.*',
+            './bower_components/bootstrap/fonts/**/*.*'
+        ],
+        html: clientApp + '**/*.html',
+        images: client + 'images/**/*.*',
+        index: client + 'index.html',
+        js: [
+            clientApp + '**/*.module.js',
+            clientApp + '**/*.js',
+            '!' + clientApp + '**/*.spec.js'
+        ],
+        sass: client + 'styles/styles.scss',
+        sassFiles: [
+            client + 'styles/*.scss'
+        ],
+        report: report,
+        root: root,
+        server: server,
+        temp: temp,
+
+        /**
+         * optimized files
+         */
+        optimized: {
+            app: 'app.js',
+            lib: 'lib.js'
+        },
+
+        /**
+         * browser sync
+         */
+        browserReloadDelay: 1000,
+
+        /**
+         * Bower and NPM locations
+         */
+        bower: {
+            json: require('./bower.json'),
+            directory: './bower_components/',
+            ignorePath: '../..',
+            // exclude: []
+        },
+        packages: [
+            './package.json',
+            './bower.json'
+        ],
+
+        /**
+         * Node settings
+         */
+        defaultPort: 7203,
+        nodeServer: './src/server/app.js'
+
+    };
+
+    config.getWiredepDefaultOptions = function () {
+        var options = {
+            bowerJson: config.bower.json,
+            directory: config.bower.directory,
+            ignorePath: config.bower.ignorePath,
+            //exclude: config.bower.exclude,
+        };
+        return options;
+    };
+
+    return config;
+
+    ////////////////
+
+};
